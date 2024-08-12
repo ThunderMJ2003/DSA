373f0e4771c8112df5a29f056ff99613ec1c328e
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2024-08-12 20:23:44 +0530

    Added A2Z_1_2_5.cpp

---
+++ DSA_Striver/A2Z/A2Z_1_2_5.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void makepattern(int n){
+    for (int i=0; i<n; i++){
+        for (int j=0; j<(n-i); j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    makepattern(n);
+    return 0;
+}
