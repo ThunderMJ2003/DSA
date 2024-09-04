e99a27aa52beba867fcb0eda2dfaf3ffc30e986a
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2024-09-04 10:40:49 +0530

    added A2Z_1_2_20.cpp

---
+++ DSA_Striver/A2Z/A2Z_1_2_20.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void makepattern(int n){
+    for (int i=0; i<n; i++){
+        for (int j=0; j<(i+1); j++){
+            printf("* ");
+        }
+        for (int j=0; j<(2*(n-i-1)); j++){
+            printf("  ");
+        }
+        for (int j=0; j<(i+1); j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for (int i=(n-2); i>=0; i--){
+        for (int j=0; j<(i+1); j++){
+            printf("* ");
+        }
+        for (int j=0; j<(2*(n-i-1)); j++){
+            printf("  ");
+        }
+        for (int j=0; j<(i+1); j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    makepattern(n);
+    return 0;
+}
