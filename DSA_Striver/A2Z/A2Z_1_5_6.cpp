b335e0956bd5c221b2558cc77a36d62196a9d43b
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2024-09-07 17:58:19 +0530

    added A2Z_1_5_6.cpp

---
+++ DSA_Striver/A2Z/A2Z_1_5_6.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    vector<long long> factorialNumbers(long long N)
+    {
+        // Write Your Code here
+        vector<long long> finarr;
+        long long i = 1;
+        long long facti = 1;
+        while(facti<=N){
+            finarr.push_back(facti);
+            ++i;
+            facti = facti * i;
+        }
+        return finarr;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long N;
+        cin>>N;
+        Solution ob;
+        vector<long long> ans = ob.factorialNumbers(N);
+        for(auto num : ans){
+            cout<<num<<" ";
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+}
+// } Driver Code Ends
