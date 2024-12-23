f3d7eee75482db5629320aefb1a47007ff4e1029
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2024-12-23 16:40:44 +0530

    updated A2Z_1_4_3.cpp

--- DSA_Striver/A2Z/A2Z_1_4_3.cpp
+++ DSA_Striver/A2Z/A2Z_1_4_3.cpp
@@ -1,17 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isPalindrome(int x){
-    if (x<0){
-        return 0;
+// bool isPalindrome(int x){
+//     if (x<0){
+//         return 0;
+//     }
+//     int ogx = x;
+//     long revx = 0;
+//     while (x>0){
+//         revx = (revx*10) + x%10;
+//         x = x/10;
+//     }
+//     return (revx==ogx);
+// }
+
+bool isPalindrome(int n) {
+    string nstr = to_string(n);
+    //cout << nstr << endl;
+    int i=0, j=nstr.size()-1;
+    while (i<j){
+        if (nstr[i] != nstr[j]){
+            return false;
+        }
+        ++i;
+        --j;
     }
-    int ogx = x;
-    long revx = 0;
-    while (x>0){
-        revx = (revx*10) + x%10;
-        x = x/10;
-    }
-    return (revx==ogx);
+    return true;
 }
 
 int main(){
