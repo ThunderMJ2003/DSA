9eeb99cd1b973790c5c27ba7f5e82627eda606d8
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2024-12-23 11:18:48 +0530

    updated A2Z_1_2_22.cpp

--- DSA_Striver/A2Z/A2Z_1_2_22.cpp
+++ DSA_Striver/A2Z/A2Z_1_2_22.cpp
@@ -1,34 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void makepattern(int n){
-    for (int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
-            printf("%d", max((n-i), (n-j)));
-        }
-        if (i<(n-1)){
-            for (int j=0; j<(n-1); j++){
-                printf("%d", max((n-i), j+2));
-            }
-        }
+// void makepattern(int n){
+//     for (int i=0; i<n; i++){
+//         for (int j=0; j<n; j++){
+//             printf("%d", max((n-i), (n-j)));
+//         }
+//         if (i<(n-1)){
+//             for (int j=0; j<(n-1); j++){
+//                 printf("%d", max((n-i), j+2));
+//             }
+//         }
         
-        if (i==(n-1)){
-            for (int j=0; j<(n-1); j++){
-                printf("%d", (j+2));
+//         if (i==(n-1)){
+//             for (int j=0; j<(n-1); j++){
+//                 printf("%d", (j+2));
+//             }
+//         }
+//         printf("\n");
+//     }
+//     for (int i=0; i<(n-1); i++){
+//         for (int j=0; j<(n-1); j++){
+//             printf("%d", max((i+2), (n-j)));
+//         }
+//         printf("%d", (i+2));
+//         for (int j=0; j<(n-1); j++){
+//             printf("%d", max((i+2), (j+2)));
+//         }
+//         printf("\n");
+//     }
+// }
+
+void makepattern(int n){
+    for (int i=0; i<(2*n-1); i++){
+            for (int j=0; j<(2*n-1); j++){
+                printf("%d", max(abs(n-1-i), abs(n-1-j))+1);
             }
+            printf("\n");
         }
-        printf("\n");
-    }
-    for (int i=0; i<(n-1); i++){
-        for (int j=0; j<(n-1); j++){
-            printf("%d", max((i+2), (n-j)));
-        }
-        printf("%d", (i+2));
-        for (int j=0; j<(n-1); j++){
-            printf("%d", max((i+2), (j+2)));
-        }
-        printf("\n");
-    }
 }
 
 int main(){
