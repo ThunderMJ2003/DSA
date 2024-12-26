8936bf59578ec62f982dddac0b31d88e2d273368
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2024-12-26 12:27:49 +0530

    updated A2Z_1_5_9.cpp

--- DSA_Striver/A2Z/A2Z_1_5_9.cpp
+++ DSA_Striver/A2Z/A2Z_1_5_9.cpp
@@ -1,11 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int fib(int n){
+// int fib(int n){
+//     if (n<2){
+//         return n;
+//     }
+//     return (fib(n-1) + fib(n-2));
+// }
+
+int fib(int n) {
     if (n<2){
         return n;
     }
-    return (fib(n-1) + fib(n-2));
+    int a=0, b=1;
+    int c = a+b;
+    for (int i=2; i<n; i++){
+        a = b;
+        b = c;
+        c = a+b;
+    }
+    return c;
 }
 
 int main(){
