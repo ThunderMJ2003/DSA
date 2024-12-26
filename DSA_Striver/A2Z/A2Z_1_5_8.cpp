596e92c0b773d7c8ee9cd3542c491f1d2659aed0
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2024-12-26 12:19:51 +0530

    updated A2Z_1_5_8.cpp

--- DSA_Striver/A2Z/A2Z_1_5_8.cpp
+++ DSA_Striver/A2Z/A2Z_1_5_8.cpp
@@ -1,12 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool recurseisPalindrome(string s){
-    int n = s.length();
-    int i = 0;
-    int j = n-1;
+// bool recurseisPalindrome(string s){
+//     int n = s.length();
+//     int i = 0;
+//     int j = n-1;
+//     while (i<j){
+//         if (s[i]!=s[j]){
+//             return false;
+//         }
+//         ++i;
+//         --j;
+//     }
+//     return true;
+// }
+
+// bool isPalindrome(string s){
+//     string cleans = "";
+//     for (int i=0; i<s.length(); i++){
+//         if (((s[i]>='a')&&(s[i]<='z')) || ((s[i]>='0')&&(s[i]<='9'))){
+//             cleans.push_back(s[i]);
+//         }
+//         if ((s[i]>='A')&&(s[i]<='Z')){
+//             cleans.push_back(s[i]-'A'+'a');
+//         }
+//     }
+//     return recurseisPalindrome(cleans);
+bool isPalindrome(string s) {
+    int i=0, j=s.size()-1;
     while (i<j){
-        if (s[i]!=s[j]){
+        while ((i<j) && (!isalnum(s[i]))){
+            ++i;
+        }
+        while ((i<j) && (!isalnum(s[j]))){
+            --j;
+        }
+        //cout << s[i] << " " << s[j] << endl;
+        if (tolower(s[i]) != tolower(s[j])){
             return false;
         }
         ++i;
@@ -15,22 +45,9 @@ bool recurseisPalindrome(string s){
     return true;
 }
 
-bool isPalindrome(string s){
-    string cleans = "";
-    for (int i=0; i<s.length(); i++){
-        if (((s[i]>='a')&&(s[i]<='z')) || ((s[i]>='0')&&(s[i]<='9'))){
-            cleans.push_back(s[i]);
-        }
-        if ((s[i]>='A')&&(s[i]<='Z')){
-            cleans.push_back(s[i]-'A'+'a');
-        }
-    }
-    return recurseisPalindrome(cleans);
-}
-
 int main(){
     string s;
-    cin >> s;
+    getline(cin, s);
     printf("%d \n", isPalindrome(s));
     return 0;
 }
