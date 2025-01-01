3dd0d241d861fd4ecddddd794b05ad1b4e2e7e1d
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2025-01-01 15:20:07 +0530

    updated A2Z_3_1_7.cpp

--- DSA_Striver/A2Z/A2Z_3_1_7.cpp
+++ DSA_Striver/A2Z/A2Z_3_1_7.cpp
@@ -8,38 +8,72 @@ using namespace std;
 // User function template for C++
 class Solution {
 public:
+    // void moveZeroes(vector<int>& nums) {
+    //     int i;
+    //     int n = nums.size();
+    //     for (i=0; i<n; i++){
+    //         if (nums[i]==0){
+    //             break;
+    //         }
+    //     }
+    //     if (i==(n-1)){
+    //         return;
+    //     }
+    //     int j;
+    //     for (j=i; j<n; j++){
+    //         if (nums[j]!=0){
+    //             break;
+    //         }
+    //     }
+    //     int t;
+    //     while (j<n){
+    //         t = nums[i];
+    //         nums[i] = nums[j];
+    //         nums[j] = t;
+    //         for (i=i+1; i<n; i++){
+    //             if (nums[i]==0){
+    //                 break;
+    //             }
+    //         }
+    //         if (i==(n-1)){
+    //             return;
+    //         }
+    //         for (j=j+1; j<n; j++){
+    //             if  (nums[j]!=0){
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
+
     void moveZeroes(vector<int>& nums) {
-        int i;
+        int i = -1;
         int n = nums.size();
-        for (i=0; i<n; i++){
-            if (nums[i]==0){
+        for (int j=0; j<n; j++){
+            if (nums[j] == 0){
+                i = j;
                 break;
             }
         }
-        if (i==(n-1)){
+        if (i == (-1)){
             return;
         }
-        int j;
-        for (j=i; j<n; j++){
-            if (nums[j]!=0){
+        int j = -1;
+        for (int k=(i+1); k<n; k++){
+            if (nums[k] != 0){
+                j = k;
                 break;
             }
         }
-        int t;
+        if (j == (-1)){
+            return;
+        }
         while (j<n){
-            t = nums[i];
-            nums[i] = nums[j];
-            nums[j] = t;
-            for (i=i+1; i<n; i++){
-                if (nums[i]==0){
-                    break;
-                }
-            }
-            if (i==(n-1)){
-                return;
-            }
-            for (j=j+1; j<n; j++){
-                if  (nums[j]!=0){
+            swap(nums[i], nums[j]);
+            ++i;
+            while (j<n){
+                ++j;
+                if ((j<n) && (nums[j] != 0)){
                     break;
                 }
             }
