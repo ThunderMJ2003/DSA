3ded2ae12748404199950cac45fe99a6442931c9
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2025-01-02 16:47:51 +0530

    A2Z_3_1_9_2.cpp

---
+++ DSA_Striver/A2Z/A2Z_3_1_9_2.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+// C++ program to find union of
+// two sorted arrays
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return a list containing the intersection of two arrays.
+    vector<int> intersection(vector<int> &arr1, vector<int> &arr2) {
+        int i=0, j=0;
+        vector<int> fin;
+        while ((i < arr1.size()) && (j < arr2.size())){
+            if (arr1[i] < arr2[j]){
+                ++i;
+            }
+            else if (arr1[i] > arr2[j]){
+                ++j;
+            }
+            else if (arr1[i] == arr2[j]){
+                if ((fin.size() == 0) || (fin.back() != arr1[i])){
+                    fin.push_back(arr1[i]);
+                }
+                ++i;
+                ++j;
+            }
+            else{
+                printf("some issue \n");
+                assert(0);
+            }
+        }
+        return fin;
+    }
+};
+
+//{ Driver Code Starts.
+/* Driver program to test above function */
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+
+        vector<int> arr1, arr2;
+        string input1;
+        getline(cin, input1);
+        stringstream ss(input1);
+        int number1;
+        while (ss >> number1) {
+            arr1.push_back(number1);
+        }
+        string input2;
+        getline(cin, input2);
+        stringstream sss(input2);
+        int number2;
+        while (sss >> number2) {
+            arr2.push_back(number2);
+        }
+        Solution ob;
+        // Function calling
+        vector<int> v;
+        v = ob.intersection(arr1, arr2);
+
+        if (v.size() > 0) {
+            for (int i = 0; i < v.size(); i++)
+                cout << v[i] << " ";
+        } else {
+            cout << "[]";
+        }
+
+        cout << endl;
+        cout << "~" << endl;
+        // string tilde;
+        // getline(cin, tilde);
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
