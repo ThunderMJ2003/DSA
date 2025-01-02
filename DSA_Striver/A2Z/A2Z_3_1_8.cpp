c76eca53c9683fd4a8cc769f92a248102c5871f6
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2025-01-02 13:53:25 +0530

    updated A2Z_3_1_8.cpp

--- DSA_Striver/A2Z/A2Z_3_1_8.cpp
+++ DSA_Striver/A2Z/A2Z_3_1_8.cpp
@@ -6,25 +6,45 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    int searchInSorted(int arr[], int N, int K) 
-    { 
-        int i = 0;
-        int j = N-1;
-        int found = -1;
-        int x;
-        while ((i<=j) && (found==-1)){
-            x = (i+j)/2;
-            if (arr[x]==K){
-                return 1;
+    // int searchInSorted(int arr[], int N, int K) 
+    // { 
+    //     int i = 0;
+    //     int j = N-1;
+    //     int found = -1;
+    //     int x;
+    //     while ((i<=j) && (found==-1)){
+    //         x = (i+j)/2;
+    //         if (arr[x]==K){
+    //             return 1;
+    //         }
+    //         else if (arr[x] < K){
+    //             i = x+1;
+    //         }
+    //         else{
+    //             j = x-1;
+    //         }
+    //     }
+    //     return found;
+    // }
+    
+    bool searchInSorted(int arr[], int n, int k) {
+        int l=0, h=(n-1);
+        while (l<=h){
+            if (arr[(l+h)/2] == k){
+                return true;
             }
-            else if (arr[x] < K){
-                i = x+1;
+            else if (arr[(l+h)/2] > k){
+                h = (l+h)/2 - 1;
+            }
+            else if (arr[(l+h)/2] < k){
+                l = (l+h)/2 + 1;
             }
             else{
-                j = x-1;
+                printf("some issue \n");
+                assert(0);
             }
         }
-        return found;
+        return false;
     }
 };
 
