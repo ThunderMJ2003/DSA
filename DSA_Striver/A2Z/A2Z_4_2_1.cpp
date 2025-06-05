a01bf34e84399cfc7267ecc80297895c788df22d
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2025-06-05 14:24:44 +0530

    A2Z_4_2_1.cpp

---
+++ DSA_Striver/A2Z/A2Z_4_2_1.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    public:
+    int mySqrt(int x){
+        int l = 0, h = x;
+        int m;
+        while (l <= h){
+            m = (l+h)/2;
+            if (((long long)m*m == x) || (((long long)m*m < x) && (((long long)m+1)*(m+1) > x))){
+                return m;
+            }
+            else if ((long long)m*m < x){
+                l = m+1;
+            }
+            else{
+                h = m-1;
+            }
+        }
+        return m;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        // int n;
+        // cin >> n;
+        // cin.ignore();
+        // vector<vector<int>> a;
+        // for (int i=0; i<n; i++){
+        //     vector<int> t;
+        //     for (int j=0; j<n; j++){
+        //         int x;
+        //         cin >> x;
+        //         t.push_back(x);
+        //     }
+        //     a.push_back(t);
+        //     t.clear();
+        // }
+
+        int n;
+        cin >> n;
+        cin.ignore();
+
+        // vector<int> a;
+        // string input;
+
+        // // Input format: first number n followed by the array elements
+        // getline(cin, input);
+        // stringstream ss(input);
+        // int num;
+        // while (ss >> num)
+        //     a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        int ans = obj.mySqrt(n);
+
+        cout << ans << endl;
+        // for (int i=0; i<ans.size(); i++){
+        //     printf("%d ", ans[i]);
+        // }
+        // for (int i=0; i<ans.size(); i++){
+        //     for (int j=0; j<ans[i].size(); j++){
+        //         cout << ans[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        cout << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
