c819c1ceea0e06021c7bab9aeb22770b0351ddbc
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2025-06-05 14:26:14 +0530

    A2Z_4_2_5.cpp

---
+++ DSA_Striver/A2Z/A2Z_4_2_5.cpp
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    public:
+    int divisorSum(vector<int>& nums, int d){
+        int fin = 0;
+        for (int i : nums){
+            fin += ceil((double)i/d);
+        }
+        return fin;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold){
+        int l = 1, h = nums[max_element(nums.begin(), nums.end()) - nums.begin()];
+        int m;
+        int tempDivisorSum;
+        while (l <= h){
+            m = (l+h)/2;
+            tempDivisorSum = divisorSum(nums, m);
+            if (tempDivisorSum <= threshold){
+                h = m-1;
+            }
+            else{
+                l = m+1;
+            }
+        }
+        return l;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        // int n;
+        // cin >> n;
+        // cin.ignore();
+        // vector<vector<int>> a;
+        // for (int i=0; i<n; i++){
+        //     vector<int> t;
+        //     for (int j=0; j<n; j++){
+        //         int x;
+        //         cin >> x;
+        //         t.push_back(x);
+        //     }
+        //     a.push_back(t);
+        //     t.clear();
+        // }
+
+        int n;
+        cin >> n;
+        cin.ignore();
+
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        int ans = obj.smallestDivisor(a, n);
+
+        cout << ans << endl;
+        // for (int i=0; i<ans.size(); i++){
+        //     printf("%d ", ans[i]);
+        // }
+        // for (int i=0; i<ans.size(); i++){
+        //     for (int j=0; j<ans[i].size(); j++){
+        //         cout << ans[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        cout << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
