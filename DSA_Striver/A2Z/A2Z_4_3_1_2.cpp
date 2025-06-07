d92c6c657b801feb7e6ad1d6000b0e00c18ee8fe
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2025-06-07 20:44:42 +0530

    A2Z_4_3_1_2.cpp

---
+++ DSA_Striver/A2Z/A2Z_4_3_1_2.cpp
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    public:
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat){
+        int n = mat.size(), m = mat[0].size();
+        int high, low, mid, max1 = 0, maxind = 0;
+        for (int i=0; i<n; i++){
+            sort(mat[i].begin(), mat[i].end());
+            low = 0, high = m-1;
+            while (low <= high){
+                mid = (high+low)/2;
+                if (((mat[i][mid] == 1) && (mid == 0)) || ((mat[i][mid] == 1) && (mat[i][mid-1] == 0))){
+                    if ((m-mid) > max1){
+                        max1 = m - mid;
+                        maxind = i;
+                    }
+                    break;
+                }
+                else if ((mat[i][mid] == 1) && (mat[i][mid-1] == 1)){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+        }
+        return {maxind, max1};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        int m;
+        cin >> m;
+        cin.ignore();
+        int n;
+        cin >> n;
+        cin.ignore();
+        vector<vector<int>> a;
+        for (int i=0; i<m; i++){
+            vector<int> t;
+            for (int j=0; j<n; j++){
+                int x;
+                cin >> x;
+                t.push_back(x);
+            }
+            a.push_back(t);
+            t.clear();
+        }
+
+        // int n;
+        // cin >> n;
+        // cin.ignore();
+
+        // vector<int> a;
+        // string input;
+
+        // // Input format: first number n followed by the array elements
+        // getline(cin, input);
+        // stringstream ss(input);
+        // int num;
+        // while (ss >> num)
+        //     a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        auto ans = obj.rowAndMaximumOnes(a);
+
+        // cout << ans << endl;
+        for (int i=0; i<ans.size(); i++){
+            printf("%d ", ans[i]);
+        }
+        // for (int i=0; i<ans.size(); i++){
+        //     for (int j=0; j<ans[i].size(); j++){
+        //         cout << ans[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        cout << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
