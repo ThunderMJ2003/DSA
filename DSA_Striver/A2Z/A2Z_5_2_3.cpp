60502ddd62ae5143719d3688bf0b37a8ddbbba37
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2025-06-14 14:21:25 +0530

    A2Z_5_2_3.cpp

---
+++ DSA_Striver/A2Z/A2Z_5_2_3.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    public:
+    int romanToInt(string s){ // O(n), O(1)
+        int fin = 0;
+        unordered_map<char, int> ctoi = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+        for (int i=0; i<s.size(); i++){
+            if ((i != (s.size()-1)) && (ctoi[s[i+1]] > ctoi[s[i]])){
+                fin -= ctoi[s[i]];
+                continue;
+            }
+            fin += ctoi[s[i]];
+        }
+        return fin;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        // int m;
+        // cin >> m;
+        // cin.ignore();
+        // int n;
+        // cin >> n;
+        // cin.ignore();
+        // vector<vector<int>> a;
+        // for (int i=0; i<m; i++){
+        //     vector<int> t;
+        //     for (int j=0; j<n; j++){
+        //         int x;
+        //         cin >> x;
+        //         t.push_back(x);
+        //     }
+        //     a.push_back(t);
+        //     t.clear();
+        // }
+
+        // int n;
+        // cin >> n;
+        // cin.ignore();
+
+        // vector<int> a;
+        // string input;
+
+        // // Input format: first number n followed by the array elements
+        // getline(cin, input);
+        // stringstream ss(input);
+        // int num;
+        // while (ss >> num)
+        //     a.push_back(num); // Read the array elements from input string
+
+        string a;
+        getline(cin, a);
+        // string b;
+        // getline(cin, b);
+        // stringstream ss(input);
+        // string st;
+        // vector<string> a;
+        // while (ss >> st)
+        //     a.push_back(st);
+
+        Solution obj;
+        auto ans = obj.romanToInt(a);
+
+        cout << ans << endl;
+        // for (int i=0; i<ans.size(); i++){
+        //     printf("%d ", ans[i]);
+        // }
+        // for (int i=0; i<ans.size(); i++){
+        //     for (int j=0; j<ans[i].size(); j++){
+        //         cout << ans[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        cout << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
