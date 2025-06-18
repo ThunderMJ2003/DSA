b4ce77697a23b670cbcb2ea35776ef51a729ae3e
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2025-06-18 15:53:04 +0530

    A2Z_6_1_2_2.cpp

---
+++ DSA_Striver/A2Z/A2Z_6_1_2_2.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// class Node{
+//     public:
+//     int data;
+//     Node* next;
+
+//     public:
+//     Node (int data1, Node* next1){
+//         data = data1;
+//         next = next1;
+//     }
+
+//     public:
+//     Node (int data1){
+//         data = data1;
+//         next = nullptr;
+//     }
+// };
+
+class ListNode {
+public:
+	int data;
+	ListNode* next;
+
+	ListNode(int data) {
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+class Solution {
+    public:
+    ListNode* addAtkthElement (ListNode* head, int k, ListNode* newElement){
+        if (k == 1){
+            newElement->next = head;
+            return newElement;
+        }
+        ListNode* iterll = head;
+        int llind = 0;
+        while (iterll != nullptr){
+            if (llind == (k-2)){
+                newElement->next = iterll->next;
+                iterll->next = newElement;
+                return head;
+            }
+            iterll = iterll->next;
+            ++llind;
+        }
+        return head;
+    }
+};
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        int m;
+        cin >> m;
+        cin.ignore();
+        int n;
+        cin >> n;
+        cin.ignore();
+        // vector<vector<int>> a;
+        // for (int i=0; i<m; i++){
+        //     vector<int> t;
+        //     for (int j=0; j<n; j++){
+        //         int x;
+        //         cin >> x;
+        //         t.push_back(x);
+        //     }
+        //     a.push_back(t);
+        //     t.clear();
+        // }
+
+        // int n;
+        // cin >> n;
+        // cin.ignore();
+
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        // string a;
+        // getline(cin, a);
+        // string b;
+        // getline(cin, b);
+        // stringstream ss(input);
+        // string st;
+        // vector<string> a;
+        // while (ss >> st)
+        //     a.push_back(st);
+
+        ListNode* head = new ListNode(a[0]);
+        ListNode* iterll = head;
+        for (int i=1; i<a.size(); i++){
+            ListNode* temp = new ListNode(a[i]);
+            iterll->next = temp;
+            iterll = iterll->next;
+        }
+        ListNode* nnode = new ListNode(n);
+
+        Solution obj;
+        auto ans = obj.addAtkthElement(head, m, nnode);
+
+        // cout << ans << endl;
+        // for (int i=0; i<ans.size(); i++){
+        //     printf("%d ", ans[i]);
+        // }
+        for (auto i=ans; i!=nullptr; i=i->next){
+            printf("%d ", i->data);
+        }
+        // for (int i=0; i<ans.size(); i++){
+        //     for (int j=0; j<ans[i].size(); j++){
+        //         cout << ans[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        cout << "~\n";
+    }
+    return 0;
+}
