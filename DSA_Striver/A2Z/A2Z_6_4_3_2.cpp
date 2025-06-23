7aedb7926790b11c9ab3746efd60b546555da0c2
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2025-06-23 18:42:42 +0530

    A2Z_6_4_3_2.cpp

---
+++ DSA_Striver/A2Z/A2Z_6_4_3_2.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// class Node{
+//     public:
+//     int data;
+//     Node* next;
+
+//     public:
+//     Node (int data1, Node* next1){
+//         data = data1;
+//         next = next1;
+//     }
+
+//     public:
+//     Node (int data1){
+//         data = data1;
+//         next = nullptr;
+//     }
+// };
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+    public:
+    ListNode* deleteDuplicates(ListNode* head){
+        if ((head == nullptr) || (head->next == nullptr)){
+            return head;
+        }
+        ListNode* lastunique = head;
+        ListNode* iterll = head->next;
+        while (iterll->next != nullptr){
+            if (iterll->val == lastunique->val){
+                ListNode* temp = iterll;
+                iterll = iterll->next;
+                delete temp;
+                continue;
+            }
+            lastunique->next = iterll;
+            lastunique = iterll;
+            iterll = iterll->next;
+        }
+        if (iterll->val == lastunique->val){
+            delete iterll;
+            lastunique->next = nullptr;
+        }
+        else{
+            lastunique->next = iterll;
+        }
+        return head;
+    }
+};
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        // int m;
+        // cin >> m;
+        // cin.ignore();
+        // int n;
+        // cin >> n;
+        // cin.ignore();
+        // vector<vector<int>> a;
+        // for (int i=0; i<m; i++){
+        //     vector<int> t;
+        //     for (int j=0; j<n; j++){
+        //         int x;
+        //         cin >> x;
+        //         t.push_back(x);
+        //     }
+        //     a.push_back(t);
+        //     t.clear();
+        // }
+
+        // int n;
+        // cin >> n;
+        // cin.ignore();
+
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        // string a;
+        // getline(cin, a);
+        // string b;
+        // getline(cin, b);
+        // stringstream ss(input);
+        // string st;
+        // vector<string> a;
+        // while (ss >> st)
+        //     a.push_back(st);
+
+        ListNode* head = new ListNode(a[0]);
+        ListNode* iterll = head;
+        for (int i=1; i<a.size(); i++){
+            ListNode* temp = new ListNode(a[i]);
+            iterll->next = temp;
+            iterll = iterll->next;
+        }
+
+        Solution obj;
+        auto ans = obj.deleteDuplicates(head);
+
+        // cout << ans << endl;
+        // for (int i=0; i<ans.size(); i++){
+        //     printf("%d ", ans);
+        // }
+        for (auto i=ans; i!=nullptr; i=i->next){
+            printf("%d ", i->val);
+        }
+        // for (int i=0; i<ans.size(); i++){
+        //     for (int j=0; j<ans[i].size(); j++){
+        //         cout << ans[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        cout << "~\n";
+    }
+    return 0;
+}
