b1a7352d69c7cd9a49c347f5d49831a2aaa02458
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2025-07-02 17:07:26 +0530

    A2Z_7_1_4.cpp

---
+++ DSA_Striver/A2Z/A2Z_7_1_4.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// class Node{
+//     public:
+//     int data;
+//     Node* next;
+
+//     public:
+//     Node (int data1, Node* next1){
+//         data = data1;
+//         next = next1;
+//     }
+
+//     public:
+//     Node (int data1){
+//         data = data1;
+//         next = nullptr;
+//     }
+// };
+
+class Solution {
+    public:
+    void SortedStackSort(stack<int>& s){
+        if (s.size() <= 1){
+            return;
+        }
+        int x = s.top();
+        s.pop();
+        SortedStackSort(s);
+        stack<int> temp;
+        while ((!s.empty()) && (s.top() > x)){
+            temp.push(s.top());
+            s.pop();
+        }
+        s.push(x);
+        while (!temp.empty()){
+            s.push(temp.top());
+            temp.pop();
+        }
+    }
+};
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        // int m;
+        // cin >> m;
+        // cin.ignore();
+        // int n;
+        // cin >> n;
+        // cin.ignore();
+        // vector<vector<int>> a;
+        // for (int i=0; i<m; i++){
+        //     vector<int> t;
+        //     for (int j=0; j<n; j++){
+        //         int x;
+        //         cin >> x;
+        //         t.push_back(x);
+        //     }
+        //     a.push_back(t);
+        //     t.clear();
+        // }
+
+        // int n;
+        // cin >> n;
+        // cin.ignore();
+
+        // vector<int> a;
+        // string input;
+
+        // // Input format: first number n followed by the array elements
+        // getline(cin, input);
+        // stringstream ss(input);
+        // int num;
+        // while (ss >> num)
+        //     a.push_back(num); // Read the array elements from input string
+
+        stack<int> a;
+        string input;
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push(num); // Read the array elements from input string
+
+        // string a;
+        // getline(cin, a);
+        // string b;
+        // getline(cin, b);
+        // stringstream ss(input);
+        // string st;
+        // vector<string> a;
+        // while (ss >> st)
+        //     a.push_back(st);
+
+        // Node* head = new Node(a[0]);
+        // Node* iterll = head;
+        // for (int i=1; i<a.size(); i++){
+        //     Node* temp = new Node(a[i]);
+        //     iterll->next = temp;
+        //     iterll = iterll->next;
+        // }
+
+        Solution obj;
+        obj.SortedStackSort(a);
+
+        while (a.size() > 0){
+            printf("%d ", a.top());
+            a.pop();
+        }
+        // cout << ans << endl;
+        // for (int i=0; i<ans.size(); i++){
+        //     printf("%d ", ans);
+        // }
+        // for (auto i=ans; i!=nullptr; i=i->next){
+        //     printf("%d ", i->data);
+        // }
+        // for (int i=0; i<ans.size(); i++){
+        //     for (int j=0; j<ans[i].size(); j++){
+        //         cout << ans[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        cout << "~\n";
+    }
+    return 0;
+}
