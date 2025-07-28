7a7113d532a35db82ecb7da215bd85503f1fb9eb
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2025-07-28 23:30:39 +0530

    A2Z_9_1_1.cpp

---
+++ DSA_Striver/A2Z/A2Z_9_1_1.cpp
@@ -0,0 +1,144 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// class Node{
+//     public:
+//     int data;
+//     Node* next;
+
+//     public:
+//     Node (int data1, Node* next1){
+//         data = data1;
+//         next = next1;
+//     }
+
+//     public:
+//     Node (int data1){
+//         data = data1;
+//         next = nullptr;
+//     }
+// };
+
+class MyStack
+{
+private:
+    int arr[1000];
+    int top;
+public:
+    MyStack(){top=-1;}
+    int pop();
+    void push(int);
+};
+void MyStack ::push(int x) {
+    ++top;
+    arr[top] = x;
+}
+// Function to remove an item from top of the stack.
+int MyStack ::pop() {
+    if (top == -1){
+        return top;
+    }
+    return arr[top--];
+}
+
+class Solution {
+    public:
+};
+
+int main() { // ignore int main, use ide for test cases check
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        // int m;
+        // cin >> m;
+        // cin.ignore();
+        // int n;
+        // cin >> n;
+        // cin.ignore();
+        // vector<vector<char>> a;
+        // for (int i=0; i<m; i++){
+        //     vector<char> t;
+        //     for (int j=0; j<n; j++){
+        //         char x;
+        //         cin >> x;
+        //         t.push_back(x);
+        //     }
+        //     a.push_back(t);
+        //     t.clear();
+        // }
+        // cin.ignore();
+
+        // double n;
+        // cin >> n;
+        // cin.ignore();
+
+        // vector<int> a;
+        // string input;
+
+        // // Input format: first number n followed by the array elements
+        // getline(cin, input);
+        // stringstream ss(input);
+        // int num;
+        // while (ss >> num)
+        //     a.push_back(num); // Read the array elements from input string
+
+        // int k;
+        // cin >> k;
+        // cin.ignore();
+        // vector<string> vecstr;
+        // string st;
+        // for (int i=0; i<k; i++){
+        //     getline(cin, st);
+        //     vecstr.push_back(st);
+        // }
+        // string st2;
+        // getline(cin, st2);
+
+        // vector<string> a;
+        // string input;
+        // // Input format: first number n followed by the array elements
+        // getline(cin, input);
+        // stringstream ss(input);
+        // string num;
+        // while (ss >> num)
+        //     a.push_back(num); // Read the array elements from input string
+
+        // string a;
+        // getline(cin, a);
+        // string b;
+        // getline(cin, b);
+        // stringstream ss(input);
+        // string st;
+        // vector<string> a;
+        // while (ss >> st)
+        //     a.push_back(st);
+
+        // Node* head = new Node(a[0]);
+        // Node* iterll = head;
+        // for (int i=1; i<a.size(); i++){
+        //     Node* temp = new Node(a[i]);
+        //     iterll->next = temp;
+        //     iterll = iterll->next;
+        // }
+
+        // Solution obj;
+        // auto ans = obj.myPow(n, k);
+
+        // cout << ans << endl;
+        // for (int i=0; i<ans.size(); i++){
+        //     cout << ans[i] << " ";
+        // }
+        // for (auto i=ans; i!=nullptr; i=i->next){
+        //     printf("%d ", i->data);
+        // }
+        // for (int i=0; i<ans.size(); i++){
+        //     for (int j=0; j<ans[i].size(); j++){
+        //         cout << ans[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        cout << "~\n";
+    }
+    return 0;
+}
